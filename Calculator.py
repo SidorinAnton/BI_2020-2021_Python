eeb9757aad1eafe8e4b5d2d3e6041516fc3efbf1
SidorinAnton <antonsidorin@list.ru>
2020-09-15 19:01:13 +0300

    Trying to follow pep 8 part2

--- Calculator.py
+++ Calculator.py
@@ -68,62 +68,63 @@ while True:
 
 
 # Hard version :) Something for the future to understand :)
-'''
-import re  # Gain regular expressions library
-while True:
-    try:
-        num1 = int(input("Enter the first digit\nOr enter 0, if your operation is unary\n"))
-        sign = input("Enter the operator\n")
-        num2 = int(input("Enter the second digit\n"))
-    except ValueError:
-        print("Something is wrong with your input\nDo you want to start again? [y/n]")
-        if input() == "y":
-            continue
-        else:
-            print("Good bye :)")
-            exit()
 
-    if num1 == 0:
-        print("Your current operation is {} {} {}".format(num1, sign, num2))
-        print("Are you sure that your operation is unary ? [y/n]")
-        if input() == "y":
-            pass
-        else:
-            print("Pleas, try again")
-            continue
-
-    try:
-        if re.match(r"[-+%*/]{1,2}", sign) and len(sign) <= 2:   # Check the operator
-            print(f"{num1} {sign} {num2} =", eval(f"{num1}{sign}{num2}"))  # Use special f-strings and eval function!!!
-            # In simple terms, eval() runs the python code (which is passed as an argument) within the program
-        else:
-            print("ERROR!!!", "'{}' is an inappropriate operator".format(sign))
-            if input("Do you want to start again? [y/n]\n") == "y":
-                continue
-            else:
-                print("Good bye :)")
-                exit()
-                
-    except SyntaxError:  # if operator begins with +,-,%,/,*, but still is not valid
-        print("ERROR!!!", "'{}' is an inappropriate operator".format(sign))
-        if input("Do you want to start again? [y/n]\n") == "y":
-            continue
-        else:
-            print("Good bye :)")
-            exit()
-            
-    except ArithmeticError:  # if it is impossible
-        print("ERROR!!!", "I can't do this operation")
-        if input("Do you want to start again? [y/n]\n") == "y":
-            continue
-        else:
-            print("Good bye :)")
-            exit()
-            
-    else:
-        if input("Do you want to start again? [y/n]\n") == "y":
-            continue
-        else:
-            print("Good bye :)")
-            exit()
-'''
+# import re  # Gain regular expressions library
+#
+# while True:
+#     try:
+#         num1 = int(input("Enter the first digit\nOr enter 0, if your operation is unary\n"))
+#         sign = input("Enter the operator\n")
+#         num2 = int(input("Enter the second digit\n"))
+#     except ValueError:
+#         print("Something is wrong with your input\nDo you want to start again? [y/n]")
+#         if input() == "y":
+#             continue
+#         else:
+#             print("Good bye :)")
+#             exit()
+#
+#     if num1 == 0:
+#         print("Your current operation is {} {} {}".format(num1, sign, num2))
+#         print("Are you sure that your operation is unary ? [y/n]")
+#         if input() == "y":
+#             pass
+#         else:
+#             print("Pleas, try again")
+#             continue
+#
+#     try:
+#         if re.match(r"[-+%*/]{1,2}", sign) and len(sign) <= 2:   # Check the operator
+#             print(f"{num1} {sign} {num2} =", eval(f"{num1}{sign}{num2}"))
+#             # Use special f-strings and eval function!!!
+#             # In simple terms, eval() runs the python code (which is passed as an argument) within the program
+#         else:
+#             print("ERROR!!!", "'{}' is an inappropriate operator".format(sign))
+#             if input("Do you want to start again? [y/n]\n") == "y":
+#                 continue
+#             else:
+#                 print("Good bye :)")
+#                 exit()
+#
+#     except SyntaxError:  # if operator begins with +,-,%,/,*, but still is not valid
+#         print("ERROR!!!", "'{}' is an inappropriate operator".format(sign))
+#         if input("Do you want to start again? [y/n]\n") == "y":
+#             continue
+#         else:
+#             print("Good bye :)")
+#             exit()
+#
+#     except ArithmeticError:  # if it is impossible
+#         print("ERROR!!!", "I can't do this operation")
+#         if input("Do you want to start again? [y/n]\n") == "y":
+#             continue
+#         else:
+#             print("Good bye :)")
+#             exit()
+#
+#     else:
+#         if input("Do you want to start again? [y/n]\n") == "y":
+#             continue
+#         else:
+#             print("Good bye :)")
+#             exit()
